1c1be56706a422272f7a3a58012bc0f0f5af362e
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-06-16 15:42:13 +0300

    Added more args for localized text widget.

--- example/lib/widgets/TextLocalized.dart
+++ example/lib/widgets/TextLocalized.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+
 import '../localization.dart';
 
 class TextLocalized extends StatelessWidget {
   final String _key;
   final TextAlign textAlign;
+  final TextStyle style;
 
-  TextLocalized(this._key, [this.textAlign]);
+  TextLocalized(this._key, {this.textAlign, this.style});
 
   @override
   Widget build(BuildContext context) {
-    return Text(AppLocalizations.instance.text(_key), textAlign: textAlign);
+    return Text(
+      AppLocalizations.instance.text(_key),
+      textAlign: textAlign,
+      style: style,
+    );
   }
 }
