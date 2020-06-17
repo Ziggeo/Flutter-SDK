8006e28c9fc61ff4c45873ae30ea68e13062fd9d
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-06-17 08:59:39 +0300

    About screen implemented.

--- example/lib/screens/about.dart
+++ example/lib/screens/about.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html/style.dart';
+import 'package:ziggeo_example/localization.dart';
+import 'package:ziggeo_example/res/colors.dart';
+import 'package:ziggeo_example/res/dimens.dart';
+import 'package:ziggeo_example/widgets/TextLocalized.dart';
 
 class AboutScreen extends StatefulWidget {
   static const String routeName = '/about';
@@ -15,9 +21,31 @@ class _AboutScreenState extends State<AboutScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(),
-    );
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: EdgeInsets.all(common_margin),
+                child: Column(
+                  children: <Widget>[
+                    TextLocalized(
+                      'about_subtitle',
+                      style: TextStyle(fontSize: about_subtitle_text_size),
+                    ),
+                    SizedBox(height: common_margin),
+                    Html(
+                      style: {
+                        "body": Style(
+                          fontSize: FontSize(about_content_text_size),
+                        ),
+                        "a": Style(
+                          // Note that the underline can be omitted, since the new parser merges styles with the element's
+                          // default style rather than replacing that style.
+                          color: Color(primary),
+                        ),
+                      },
+                      data: AppLocalizations.instance.text('about_text'),
+                    ),
+                  ],
+                ))));
   }
-
 }
