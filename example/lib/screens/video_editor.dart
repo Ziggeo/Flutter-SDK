0975871eb627ecc80c0f4eead8c1be486d330832
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-06-18 09:52:51 +0300

    VideoEditor screen implemented.

--- example/lib/screens/video_editor.dart
+++ example/lib/screens/video_editor.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:ziggeo_example/widgets/TextLocalized.dart';
 
-class VideoEditorScreen extends StatefulWidget {
+class VideoEditorScreen extends StatelessWidget {
   static const String routeName = '/video-editor';
 
   @override
-  _VideoEditorScreenState createState() => _VideoEditorScreenState();
-}
-
-class _VideoEditorScreenState extends State<VideoEditorScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(),
+    return Scaffold(
+      body: Align(
+        child: TextLocalized('coming_soon'),
+        alignment: Alignment.center,
+      ),
     );
   }
-
 }
