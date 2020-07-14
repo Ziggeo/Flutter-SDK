98e5acba5f8b7ee59678fbd36d79c4e44edb8385
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-07-14 11:18:35 +0300

    Added validation for input field for auth screen.

--- example/lib/screens/auth.dart
+++ example/lib/screens/auth.dart
@@ -8,6 +8,8 @@ import 'package:ziggeo_example/res/dimens.dart';
 import 'package:ziggeo_example/screens/main.dart';
 import 'package:ziggeo_example/widgets/TextLocalized.dart';
 
+import '../localization.dart';
+
 class AuthScreen extends StatefulWidget {
   @override
   _AuthScreenState createState() => _AuthScreenState();
@@ -17,6 +19,7 @@ class _AuthScreenState extends State<AuthScreen> {
   bool _enterQrManuallyMode = false;
   Ziggeo _ziggeo;
   String _inputToken;
+  final _formKey = GlobalKey<FormState>();
 
   _AuthScreenState() {
     _ziggeo = Ziggeo(_inputToken);
@@ -28,7 +31,9 @@ class _AuthScreenState extends State<AuthScreen> {
 
   onQrActionPressed() {
     if (_enterQrManuallyMode) {
-      navigateToMainScreen(_inputToken);
+      if (_formKey.currentState.validate()) {
+        navigateToMainScreen(_inputToken);
+      }
     } else {
       _ziggeo.startQrScanner();
     }
@@ -56,50 +61,59 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
         body: Padding(
       padding: EdgeInsets.all(common_margin),
-      child: Center(
-          child: Column(
-        children: <Widget>[
-          SizedBox(height: logo_margin_top),
-          Image.asset(
-            'assets/img/logo.png',
-            width: logo_width,
-          ),
-          SizedBox(height: logo_margin_bottom),
-          TextLocalized('auth_message', textAlign: TextAlign.center),
-          SizedBox(height: auth_controls_margin_top),
-          SizedBox(
-            height: qr_input_height,
-            child: _enterQrManuallyMode
-                ? TextFormField(
-                    onChanged: (value) => this.setState(() {
-                      _inputToken = value;
-                    }),
-                    decoration:
-                        InputDecoration(labelText: 'Enter your username'),
-                  )
-                : null,
-          ),
-          SizedBox(height: common_margin),
-          SizedBox(
-              width: btn_qr_width,
-              child: RaisedButton(
-                  onPressed: () => this.onQrActionPressed(),
-                  child: TextLocalized(
-                    _enterQrManuallyMode
-                        ? 'btn_use_entered_text'
-                        : 'btn_scan_qr_text',
-                  ))),
-          SizedBox(
-              width: btn_qr_width,
-              child: FlatButton(
-                  onPressed: () => this.switchQrScannerMode(),
-                  child: TextLocalized(
-                    _enterQrManuallyMode
-                        ? 'use_scanner_text'
-                        : 'enter_qr_manually_text',
-                  ))),
-        ],
-      )),
+      child: Form(
+          key: _formKey,
+          child: Center(
+              child: Column(
+            children: <Widget>[
+              SizedBox(height: logo_margin_top),
+              Image.asset(
+                'assets/img/logo.png',
+                width: logo_width,
+              ),
+              SizedBox(height: logo_margin_bottom),
+              TextLocalized('auth_message', textAlign: TextAlign.center),
+              SizedBox(height: auth_controls_margin_top),
+              SizedBox(
+                height: qr_input_height,
+                child: _enterQrManuallyMode
+                    ? TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return AppLocalizations.instance
+                                .text('err_not_empty');
+                          }
+                          return null;
+                        },
+                        onChanged: (value) => this.setState(() {
+                          _inputToken = value;
+                        }),
+                        decoration:
+                            InputDecoration(labelText: 'Enter your username'),
+                      )
+                    : null,
+              ),
+              SizedBox(height: common_margin),
+              SizedBox(
+                  width: btn_qr_width,
+                  child: RaisedButton(
+                      onPressed: () => this.onQrActionPressed(),
+                      child: TextLocalized(
+                        _enterQrManuallyMode
+                            ? 'btn_use_entered_text'
+                            : 'btn_scan_qr_text',
+                      ))),
+              SizedBox(
+                  width: btn_qr_width,
+                  child: FlatButton(
+                      onPressed: () => this.switchQrScannerMode(),
+                      child: TextLocalized(
+                        _enterQrManuallyMode
+                            ? 'use_scanner_text'
+                            : 'enter_qr_manually_text',
+                      ))),
+            ],
+          ))),
     ));
   }
 }
