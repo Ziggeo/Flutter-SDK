885052ac070e410b10f440c5502916f71e185643
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-07-17 10:12:31 +0300

    Added more api methods for videos service.

--- android/src/main/kotlin/com/ziggeo/flutterplugin/api/VideosMethodChannel.kt
+++ android/src/main/kotlin/com/ziggeo/flutterplugin/api/VideosMethodChannel.kt
@@ -1,9 +1,13 @@
 package com.ziggeo.flutterplugin.api
 
+import android.annotation.SuppressLint
 import com.ziggeo.androidsdk.Ziggeo
+import com.ziggeo.androidsdk.net.models.videos.VideoModel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.reactivex.Completable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
@@ -17,20 +21,57 @@ class VideosMethodChannel(private val ziggeo: Ziggeo) : MethodCallHandler {
                 (call.arguments as? HashMap<String, String>)?.let {
                     args = it
                 }
-                ziggeo.apiRx().videosRaw()
-                        .index(args)
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe { json, throwable ->
-                            if (throwable != null) {
-                                result.error(call.method, throwable.toString(), throwable.message)
-                            } else json?.let {
-                                result.success(json)
-                            }
-                        }
+                processRequest(ziggeo.apiRx().videosRaw()
+                        .index(args), call, result)
+            }
+            "get" -> call.argument<String>("videoToken")?.let {
+                processRequest(ziggeo.apiRx().videosRaw()[it], call, result)
+            }
+            "getVideoUrl" -> call.argument<String>("videoToken")?.let {
+                processRequest(Single.fromCallable { ziggeo.apiRx().videosRaw().getVideoUrl(it) }, call, result)
+            }
+            "getImageUrl" -> call.argument<String>("videoToken")?.let {
+                processRequest(Single.fromCallable { ziggeo.apiRx().videosRaw().getImageUrl(it) }, call, result)
+            }
+            "update" -> call.argument<String>("data")?.let {
+                processRequest(ziggeo.apiRx().videosRaw().update(VideoModel.fromJson(it)), call, result)
+            }
+            "destroy" -> call.argument<String>("videoToken")?.let {
+                processRequest(ziggeo.apiRx().videosRaw().destroy(it), call, result)
+            }
+            "create" -> {
+                var args: HashMap<String, String>? = null
+                (call.arguments as? HashMap<String, String>)?.let {
+                    args = it
+                }
+                processRequest(ziggeo.apiRx().videosRaw()
+                        .create(args), call, result)
+            }
+            "createWithFile" -> {
             }
             else -> result.notImplemented()
         }
     }
 
+    @SuppressLint("CheckResult")
+    private fun processRequest(request: Single<*>, call: MethodCall, result: MethodChannel.Result) {
+        request.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { json, throwable ->
+                    if (throwable != null) {
+                        result.error(call.method, throwable.toString(), throwable.message)
+                    } else json?.let {
+                        result.success(json)
+                    }
+                }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun processRequest(request: Completable, call: MethodCall, result: MethodChannel.Result) {
+        request.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    result.success(null)
+                }, { throwable: Throwable? -> result.error(call.method, throwable.toString(), throwable?.message) })
+    }
 }
--- lib/api/videos.dart
+++ lib/api/videos.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 
 class VideosApi {
@@ -7,5 +9,33 @@ class VideosApi {
 
   Future<String> index(Map<String, String> args) async {
     return await _videosChannel.invokeMethod('index', {'args': args});
+  }
+
+  Future<String> get(String videoTokenOrKey) async {
+    return await _videosChannel
+        .invokeMethod('get', {'videoToken': videoTokenOrKey});
+  }
+
+  Future<String> getVideoUrl(String videoTokenOrKey) async {
+    return await _videosChannel
+        .invokeMethod('getVideoUrl', {'videoToken': videoTokenOrKey});
+  }
+
+  Future<String> getImageUrl(String videoTokenOrKey) async {
+    return await _videosChannel
+        .invokeMethod('getImageUrl', {'videoToken': videoTokenOrKey});
+  }
+
+  Future<String> update(String json) async {
+    return await _videosChannel.invokeMethod('update', {'data': json});
+  }
+
+  Future<String> destroy(String videoTokenOrKey) async {
+    return await _videosChannel
+        .invokeMethod('destroy', {'videoToken': videoTokenOrKey});
+  }
+
+  Future<String> create(Map<String, String> args) async {
+    return await _videosChannel.invokeMethod('create', {'args': args});
   }
 }
