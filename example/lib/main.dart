6f880bb2ef367b4c7fdcc555db414befbce6c8ff
Alex Bedulin <alexander.bedulin@gmail.com>
2021-01-05 17:45:02 +0300

    Revert "Fixed crashlitics for firebase"
    This reverts commit b394d06a

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
@@ -7,18 +5,10 @@ import 'package:provider/single_child_widget.dart';
 import 'package:ziggeo_example/res/colors.dart';
 import 'package:ziggeo_example/screens/drawer.dart';
 import 'package:ziggeo_example/screens/splash.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'localization.dart';
 
-void main() async {
-  if (kDebugMode) {
-    FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
-  }
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(ZiggeoDemoApp());
-}
+void main() => runApp(ZiggeoDemoApp());
 
 class ZiggeoDemoApp extends StatefulWidget {
   @override
@@ -28,7 +18,6 @@ class ZiggeoDemoApp extends StatefulWidget {
 class _ZiggeoDemoAppState extends State<ZiggeoDemoApp> {
   @override
   Widget build(BuildContext context) {
-    FirebaseCrashlytics.instance.crash();
     return MultiProvider(
       child: MaterialApp(
         home: SplashScreen(),
@@ -42,7 +31,9 @@ class _ZiggeoDemoAppState extends State<ZiggeoDemoApp> {
           const Locale('en'),
         ],
         theme: ThemeData.light().copyWith(
-          textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Quicksand'),
+          textTheme: Theme.of(context)
+              .textTheme
+              .apply(fontFamily: 'Quicksand'),
           primaryTextTheme: Theme.of(context)
               .textTheme
               .apply(fontFamily: 'Quicksand', bodyColor: Colors.white),
