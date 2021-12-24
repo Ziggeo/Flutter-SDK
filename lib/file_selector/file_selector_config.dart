0ad13ec3a9c969a33a2e154878cb3546c1702fd1
Alex Bedulin <alexander.bedulin@gmail.com>
2021-12-24 10:48:50 +0300

    added field to file selector config

--- lib/file_selector/file_selector_config.dart
+++ lib/file_selector/file_selector_config.dart
@@ -2,8 +2,13 @@ import 'package:ziggeo/common/base_config.dart';
 import 'package:ziggeo/file_selector/file_selector_listener.dart';
 
 class FileSelectorConfig extends BaseConfig {
+  static const videoMediaType = 0x01;
+  static const audioMediaType = 0x02;
+  static const imageMediaType = 0x04;
+
   var maxDuration = 0;
   var shouldAllowMultipleSelection = false;
+  var mediaType = videoMediaType;
 
   FileSelectorEventsListener? eventsListener;
 
@@ -12,6 +17,7 @@ class FileSelectorConfig extends BaseConfig {
     var map = super.convertToMap();
     map["maxDuration"] = maxDuration;
     map["shouldAllowMultipleSelection"] = shouldAllowMultipleSelection;
+    map["mediaType"] = mediaType;
     return map;
   }
 }
